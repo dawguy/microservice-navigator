3945d7bf851627bd305a0d6a25f6ef3ff1425b81
David Wright <hockeyguy411@gmail.com>
2022-06-10 18:15:27 -0400

    CLI I/O and generates list of properly formatted URLs to load

--- src/microservice_navigator.clj
+++ src/microservice_navigator.clj
@@ -5,20 +5,36 @@
                                  (first (drop (- (count path) 2) path))))
 (defn get-files [] (mapv identity (.list (clojure.java.io/file "./"))))
 
-(def targets ["http://www.github.com/dawguy/aaa" "http://www.github.com/dawguy/bbb" "http://www.github.com/dawguy/ccc"])
-(def files ["abca" "1234" "ghjj"])
-
-
+; Goal for near future is to load this list from SQLite. Goal is to map what the microservice directories are called with
+; the source control host's website for them
 (def microservice-mapping
   {"abc" "http://www.github.com/dawguy/aaa"
    "def" "http://www.github.com/dawguy/zzz"
-   "ghj" "http://www.github.com/dawguy/123"})
+   "ghj" "http://www.github.com/dawguy/123"
+   ;"microservice-navigator" "http://www.github.com/dawguy/microservice-navigator" ; Uncomment to test being called from within top-level directory of microservice
+   ".nrepl-port" "https://github.com/dawguy/websockets-tictactoe"
+   "LICENSE.txt" "https://github.com/dawguy/websockets-tictactoe-client"
+   })
 
-(def sub-options
-  {"Home" "/"
-   "Merge requests" "/pull-request"
-   "Environment" "/environment"
-   "Pipeline" "/pipeline"})
+; Not that many options so these can be hard-coded lists.
+; Sub-options describe which function the user wants to load into.
+(def gitlab-sub-options [{:label "Todo" :path "/Todo"}])
+(def github-sub-options
+  [{:label "Home" :path "/"}
+   {:label "Issues" :path "/issues"}
+   {:label "Pull Requests" :path "/pulls"}
+   {:label "Actions" :path "/actions"} ])
+(def sub-options github-sub-options)                        ; Swap this out depending on what is being targeted.
+
+;-----------------
+; Helpful defs. Will move these to comment once they start being changed less
+(def targets ["http://www.github.com/dawguy/aaa" "http://www.github.com/dawguy/bbb" "http://www.github.com/dawguy/ccc"])
+(def files ["abc" "1234" "ghj"])
+(def files [".nrepl-port" "deps.edn" "microservice-navigator.iml" "LICENSE.txt" ".gitignore" ".idea" "README.md" ".cpcache" ".git" "test" "src"])
+(def current-directory (get-current-directory))
+(def selected-option "2")
+(def matched-microservices (map #(get microservice-mapping %) (filter #(contains? microservice-mapping %) files)))
+;-----------------
 
 (defn provide-options [targets]
   (if (empty? targets)
@@ -26,11 +42,11 @@
       (do
         (prn "-----------------------------------------")
         (prn "Select the microservice you wish to open:")
-        (prn "1: All")
-        (for [i (range 2 (+ (count targets) 2))
-              :let [t (get targets (- i 2))]]
-          (.write *out* (str i ": " t)))
-        (.read *in*)
+        (println (apply str "1: All\n"
+                  (for [i (range 0 (count targets))
+                        :let [t (nth targets i)]]
+                    (str (+ i 2) ": " t (if (not= (count targets) (inc i)) "\n")))))
+        (read-line)
         )))
 
 (defn provide-sub-options [targets]
@@ -39,21 +55,49 @@
     (do
       (prn "-----------------------------------------")
       (prn "Select the page you want to open:")
-      (for [i (range 1 (inc (count sub-options)))
-            o (keys sub-options)]
-        (prn (str i ": " o))))
-    ))
+      (println (apply str
+                      (for [i (range 0 (count sub-options))
+                            :let [o (nth sub-options i)]]
+                        (str (inc i) ": " (:label o) (if (not= (count sub-options) (inc i)) "\n")))))
+      (let [selected-sub-option (read-line)]
+        (if (string? selected-sub-option) (:path (nth sub-options (Integer/parseInt selected-sub-option))) nil)
+        )
+      )))
 
 (defn matching-microservices [files]
   (let [current-directory (get-current-directory)]
-    (if (contains? files current-directory)
-      (provide-sub-options [current-directory])
-      (provide-options (map #(get microservice-mapping %) (filter #(contains? microservice-mapping %) files))))))
+    (if (contains? microservice-mapping current-directory)
+      [[(get microservice-mapping current-directory)] (provide-sub-options [current-directory])]
+      (let [matched-microservices (mapv #(get microservice-mapping %) (filter #(contains? microservice-mapping %) files))
+            selected-option (provide-options matched-microservices)
+            parsed-int (if (string? selected-option) (- (Integer/parseInt selected-option) 2) nil)
+            dirs (case selected-option
+                   "1" matched-microservices                ; 1 represents all
+                   (if (nil? parsed-int) nil (vector (nth matched-microservices parsed-int)))
+                   )
+            ]
+          (if (nil? dirs) nil [dirs (provide-sub-options dirs)])
+        )
+      )))
+
+(defn gen-list [o]
+  (map #(str % (second o)) (first o)))
+
+(defn open-in-browser [s] (println s))
 
 ; Idea. Make sure the option for branch is allowed
 (defn run [& opts]
-  (prn (matching-microservices (get-files)))
-  (prn (str "The line you wrote was " (read-line)))
+  (loop [p (-> (get-files)
+               (matching-microservices)
+               (gen-list))]
+    (if (empty? p) p
+                 (do
+                   (open-in-browser (first p))
+                   (recur (rest p)))))
   )
 
 (run)
+
+(comment
+  (matching-microservices files)
+)
